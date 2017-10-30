7f02f9d719698536535107a90b589fc7ad5bdcbc
Elias Agg <elias.aggoune@elias-agg.com>
2017-10-30 14:01:49 +0100

    fonction click()

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-$("div").fadeout();
+ $("#cercle").click(function(){
+        alert("Vous avez cliqué sur le cercle.");
+});
+            
+$(".carre").click(function(){
+        alert("Vous avez cliqué sur un carré.");
+});
