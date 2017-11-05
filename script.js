c1b4280a12322a33c0a7b6b04f96b6ecc34b476b
Elias Agg <elias.aggoune@elias-agg.com>
2017-11-05 08:49:29 +0100

    What the fuck

--- script.js
+++ script.js
@@ -1,7 +1,112 @@
- $("#cercle").click(function(){
-        alert("Vous avez cliqué sur le cercle.");
+var isFinished = 1;
+
+$(".logo").click(function(){
+   $(this).animate({
+       "position":"relative",
+       "bottom":"800px",
+   }, 800);
+    $(this).animate({
+        "position":"relative",
+        "bottom":"0px",
+   }, 3000);
 });
-            
-$(".carre").click(function(){
-        alert("Vous avez cliqué sur un carré.");
+
+$(".logo").mouseenter(function(){
+    $(this).animate({
+        "width":"105px",
+        "height":"105px",
+    }, 50);
+    $(this).animate({
+        "width":"100px",
+        "height":"100px",
+    }, 50);
+})
+
+function blinkC(){
+    $("#blinking").animate({"opacity":"1"}, 500);
+    $("#blinking").animate({"opacity":"0"}, 500);
+    window.setTimeout(function() { blinkC() }, 500)
+//    $("#blinking").show(100);
+}
+
+$(document).ready(function() {
+    blinkC();  
 });
+
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
+
+$(function () {
+
+  showText("#targetText", "Mission to Mars", 0, 50);   
+
+});
+
+function showTextYo(target, message, index, interval){
+    if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
+
+// Change le texte du "logo" quand hover nav
+$("#nav1").mouseenter(
+    function(){
+        document.getElementById("targetText").innerHTML = " ";
+        showTextYo("#targetText", "navigation #1", 0, 50);
+    }
+).mouseleave(
+    function(){
+        setTimeout(function(){
+            document.getElementById("targetText").innerHTML = "";
+            showTextYo("#targetText", "Mission to Mars", 0, 50);
+        }, 800)
+    }
+);
+
+$("#nav2").mouseenter(
+    function(){
+        document.getElementById("targetText").innerHTML = " ";
+        showTextYo("#targetText", "navigation #2", 0, 50);
+    
+    }
+).mouseleave(
+    function(){
+        setTimeout(function(){
+            document.getElementById("targetText").innerHTML = "";
+            showTextYo("#targetText", "Mission to Mars", 0, 50);
+        }, 800)
+    }
+);
+
+$("#nav3").mouseenter(
+    function(){
+        document.getElementById("targetText").innerHTML = " ";
+        showTextYo("#targetText", "navigation #3", 0, 50);
+    }
+).mouseleave(
+    function(){
+        setTimeout(function(){
+            document.getElementById("targetText").innerHTML = "";
+            showTextYo("#targetText", "Mission to Mars", 0, 50);
+        }, 800)
+    }
+);
+
+$("#nav4").mouseenter(
+    function(){
+        document.getElementById("targetText").innerHTML = " ";
+        showTextYo("#targetText", "navigation #4", 0, 50);
+    }
+).mouseleave(
+    function(){
+        setTimeout(function(){
+            document.getElementById("targetText").innerHTML = "";
+            showTextYo("#targetText", "Mission to Mars", 0, 50);
+        }, 800)
+    }
+);
